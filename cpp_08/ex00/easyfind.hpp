a02e109c0f062cebfd982834f99d3beff0c07f8a
Elif Ebrar Kavak <elifebrarkavak@gmail.com>
2026-04-17 11:48:31 +0300

    ex00 edited

--- cpp_08/ex00/easyfind.hpp
+++ cpp_08/ex00/easyfind.hpp
@@ -2,25 +2,13 @@
 #define EASYFIND_HPP
 
 #include <algorithm>
-#include <exception>
-
-class NotFoundException : public std::exception
-{
-    public:
-        virtual const char* what() const throw()
-        {
-            return "Element Not Found";
-        }
-};
 
 template <typename T>
 typename T::iterator easyfind(T& container, int value)
 {
     typename T::iterator it = std::find(container.begin(), container.end(), value);
     if (it == container.end())
-    {
-        throw NotFoundException();
-    }
+        throw std::out_of_range("Error: Value not found in container!!!");
     return it;
 }
 
