65734003a3d7754b9244e6890d2b0bcd81cd9579
Elif Ebrar Kavak <elifebrarkavak@gmail.com>
2026-04-18 16:57:39 +0300

    ex02 main edited

--- cpp_08/ex02/MutantStack.hpp
+++ cpp_08/ex02/MutantStack.hpp
@@ -2,11 +2,17 @@
 #define MUTANTSTACK_HPP
 
 #include <stack>
+#include <deque>
 
 template <typename T>
 class MutantStack : public std::stack<T>
 {
     public:
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+        typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+        typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+
         MutantStack() : std::stack<T>()
         {
 
@@ -26,10 +32,8 @@ class MutantStack : public std::stack<T>
 
         ~MutantStack()
         {
-            
-        }
 
-        typedef typename std::stack<T>::container_type::iterator iterator;
+        }
 
         iterator begin()
         {
@@ -39,6 +43,36 @@ class MutantStack : public std::stack<T>
         iterator end()
         {
             return this->c.end();
+        }
+
+        const_iterator begin() const
+        {
+            return this->c.begin();
+        }
+
+        const_iterator end() const
+        {
+            return this->c.end();
+        }
+
+        reverse_iterator rbegin()
+        {
+            return this->c.rbegin();
+        }
+
+        reverse_iterator rend()
+        {
+            return this->c.rend();
+        }
+
+        const_reverse_iterator rbegin() const
+        {
+            return this->c.rbegin();
+        }
+
+        const_reverse_iterator rend() const
+        {
+            return this->c.rend();
         }
 };
 
