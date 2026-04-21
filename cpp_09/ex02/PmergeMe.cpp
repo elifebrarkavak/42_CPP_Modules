85e611b61965356b2ea47b330d5c0c79982c66b5
Elif Ebrar Kavak <elifebrarkavak@gmail.com>
2026-04-21 22:10:26 +0300

    atoi fixed

--- cpp_09/ex02/PmergeMe.cpp
+++ cpp_09/ex02/PmergeMe.cpp
@@ -1,19 +1,18 @@
 #include "PmergeMe.hpp"
-#include <sys/time.h> // gettimeofday için gerekli
+#include <sys/time.h>
+#include <cstdlib>
 
 PmergeMe::PmergeMe()
 {
-
 }
 
 PmergeMe::~PmergeMe()
 {
-
 }
 
 PmergeMe::PmergeMe(const PmergeMe &other)
 {
-     *this = other;
+    *this = other;
 }
 
 PmergeMe &PmergeMe::operator=(const PmergeMe &other)
@@ -35,51 +34,74 @@ bool PmergeMe::isPositiveInteger(const std::string &str)
     return true;
 }
 
-void PmergeMe::sortVector(int argc, char **argv) {
+void PmergeMe::sortVector(int argc, char **argv)
+{
     std::vector<int> v;
     struct timeval start, end;
 
-    // Zaman ölçümü verilerin işlenmesiyle başlar 
-    gettimeofday(&start, NULL);
-    for (int i = 1; i < argc; i++) {
-        if (!isPositiveInteger(argv[i])) throw std::runtime_error("Error");
-        v.push_back(std::atoi(argv[i]));
+    for (int i = 1; i < argc; i++)
+    {
+        if (!isPositiveInteger(argv[i]))
+            throw std::runtime_error("Error");
+
+        char *endptr;
+        long value = std::strtol(argv[i], &endptr, 10);
+
+        if (*endptr != '\0')
+            throw std::runtime_error("Error");
+
+        v.push_back(static_cast<int>(value));
     }
 
     std::cout << "Before: ";
-    for (size_t i = 0; i < v.size(); i++) std::cout << v[i] << " ";
+    for (size_t i = 0; i < v.size(); i++)
+        std::cout << v[i] << " ";
     std::cout << std::endl;
 
+    gettimeofday(&start, NULL);
     fordJohnsonSort(v);
     gettimeofday(&end, NULL);
 
     std::cout << "After:  ";
-    for (size_t i = 0; i < v.size(); i++) std::cout << v[i] << " ";
+    for (size_t i = 0; i < v.size(); i++)
+        std::cout << v[i] << " ";
     std::cout << std::endl;
 
-    // Mikro saniye hesaplama
-    long seconds = end.tv_sec - start.tv_sec;
-    long microseconds = end.tv_usec - start.tv_usec;
-    double elapsed = seconds * 1e6 + microseconds;
+    double elapsed = (end.tv_sec - start.tv_sec) * 1e6
+                   + (end.tv_usec - start.tv_usec);
 
-    std::cout << "Time to process " << v.size() << " elements with std::vector : " << elapsed << " us" << std::endl;
+    std::cout << "Time to process " << v.size()
+              << " elements with std::vector : "
+              << elapsed << " us" << std::endl;
 }
 
-void PmergeMe::sortDeque(int argc, char **argv) {
+void PmergeMe::sortDeque(int argc, char **argv)
+{
     std::deque<int> d;
     struct timeval start, end;
 
-    gettimeofday(&start, NULL);
-    for (int i = 1; i < argc; i++) {
-        d.push_back(std::atoi(argv[i]));
+    for (int i = 1; i < argc; i++)
+    {
+        if (!isPositiveInteger(argv[i]))
+            throw std::runtime_error("Error");
+
+        char *endptr;
+        long value = std::strtol(argv[i], &endptr, 10);
+
+        if (*endptr != '\0')
+            throw std::runtime_error("Error");
+
+        d.push_back(static_cast<int>(value));
     }
 
+    gettimeofday(&start, NULL);
     fordJohnsonSort(d);
     gettimeofday(&end, NULL);
 
-    long seconds = end.tv_sec - start.tv_sec;
-    long microseconds = end.tv_usec - start.tv_usec;
-    double elapsed = seconds * 1e6 + microseconds;
+    double elapsed = (end.tv_sec - start.tv_sec) * 1e6
+                   + (end.tv_usec - start.tv_usec);
 
-    std::cout << "Time to process " << d.size() << " elements with std::deque  : " << elapsed << " us" << std::endl;
+    std::cout << "Time to process " << d.size()
+              << " elements with std::deque  : "
+              << elapsed << " us" << std::endl;
 }
